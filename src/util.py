79d053eb5cd07a9ed49936108504bbd66f28713f
Eduardo Ferraz <dudiferraz@gmail.com>
2025-09-25 00:16:33 -0300

    refactored util functions

--- src/util.py
+++ src/util.py
@@ -1,61 +1,55 @@
 import os
 import shutil
 import re
-from dotenv import load_dotenv
+import time
 
-def move_files(path_: str, begin_year_: int, end_year_: int) -> None:
-    files_in_dir = os.listdir(path_)
-
-    for y in range(begin_year_, end_year_+1):
-        for m in range(1, 13):
-            m_str = f"{m:02d}"
-            pattern_str = f"^g\\d+_xrs_1m_3s_{y}{m_str}01_{y}{m_str}\\d{{2}}\\.csv$"
-            pattern = re.compile(pattern_str)
-
-            found_file_name = None
-            for file_name in files_in_dir:
-                if pattern.match(file_name):
-                    found_file_name = file_name
-                    break
-
-            if found_file_name is None:
-                print(f"ERRO: Arquivo não encontrado, pulando mes {m} de {y}")
-                continue
-
-            full_path = os.path.join(path_, found_file_name)
-            destiny_path = os.path.join(path_, f"{y}")
-            shutil.move(full_path, os.path.join(destiny_path, found_file_name))
-
-    return None
-
-def check_files(path_: str, begin_year_: int, end_year_: int) -> None:
-    for y in range(begin_year_, end_year_+1):
-        year_dir = os.path.join(path_, str(y))
-        files_in_dir = os.listdir(year_dir)
-        for m in range(1, 13):
-            m_str = f"{m:02d}"
-
-            if y <= 1985:
-                pattern_str = f"^g\\d+_xrs_1m_3s_{y}{m_str}01_{y}{m_str}\\d{{2}}\\.csv$"
-                pattern = re.compile(pattern_str)
-            else:
-                pattern_str = f"^g\\d+_xrs_1m_{y}{m_str}01_{y}{m_str}\\d{{2}}\\.csv$"
-                pattern = re.compile(pattern_str)
-
-            match = False
-            for file in files_in_dir:
-                if pattern.match(file):
-                    match = True
-                    break
-            if not match:
-                print(f"AVISO: Arquivo faltante! {m} de {y}")
-
-    return None
-
-def create_dirs(path_: str, begin_year_: int, end_year_: int) -> None:
-
-    for y in range(begin_year_,end_year_+1):
-        year_dir = os.path.join(path_, str(y))
+def create_dirs(path: str, range_:range) -> None:
+    for i in range_:
+        year_dir = os.path.join(path, str(i))
         os.makedirs(year_dir, exist_ok=True)
+
+    return None
+
+
+def move_file(origin_dir: str, destiny_dir: str, file_name: str | re.Pattern[str]) -> None:
+    files_in_dir = os.listdir(origin_dir)
+    found_file_name = None
+    for file in files_in_dir:
+        if isinstance(file_name, re.Pattern):
+            if file_name.match(file):
+                found_file_name = file
+                break
+        else:
+            if file==file_name:
+                found_file_name = file
+                break
+    try:
+        full_origin_path = os.path.join(origin_dir, found_file_name)
+        full_destiny_path = os.path.join(destiny_dir, found_file_name)
+        shutil.move(full_origin_path, full_destiny_path)
+    except TypeError as e:
+        print(f'FILE ({file_name}) NOT FOUND')
+
+    return None
+
+
+def wait_download(file_path_, file_name_, timeout_seconds = 300) -> None:
+    part_file_path = file_path_ + '.part'
+    start_time = time.time()
+    download_finished = False
+
+    print(f"        -> Aguardando conclusão do download...")
+    while time.time() - start_time < timeout_seconds:
+        if not os.path.exists(part_file_path):
+            time.sleep(1)
+            if os.path.exists(file_path_):
+                print(f"        -> Download de '{file_name_}' concluído com sucesso!")
+                download_finished = True
+                break
+
+        time.sleep(1)
+
+    if not download_finished:
+        print(f"        -> ERRO: Timeout! O download de '{file_name_}' demorou mais de {timeout_seconds}s.")
 
     return None
